b7134e4bff88df3eb4def4bd1af5f3e20b9437a9
CT08859 <vaibhav.jain2@citiustech.com>
2024-01-08 18:24:51 +0530

    Added PostController with dummy api

--- src/main/java/com/citiustech/blog/controller/PostController.java
+++ src/main/java/com/citiustech/blog/controller/PostController.java
@@ -1,27 +1,41 @@
 package com.citiustech.blog.controller;
 
+import com.citiustech.blog.model.BlogDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PostController {
 
+    @PostMapping("/post")
+    public ResponseEntity<Object> addPost(@RequestBody BlogDto blogDto){
+        return new ResponseEntity<>("Post Added Successfully", HttpStatus.CREATED);
+    }
+
+
     @GetMapping("/post")
     public ResponseEntity<Object> getAllPost(){
-        return new ResponseEntity<>("3:35 Pm from here yyou find all Post", HttpStatus.ACCEPTED);
-
+        return new ResponseEntity<>("3:35 Pm started, 4:10Pm edit started agan 4:30PM", HttpStatus.ACCEPTED);
     }
 
-    @DeleteMapping("post/id")
-    public ResponseEntity<Object> deletePostById(@PathVariable("id") String id){
-        return new ResponseEntity<>("Deleted Succesfully", HttpStatus.OK);
-
-
+    @PutMapping("/post/{id}")
+    public ResponseEntity<Object> updatePostById(@PathVariable Integer id){
+        return new ResponseEntity<>("Post updated successfully", HttpStatus.ACCEPTED);
     }
 
+    @DeleteMapping("/post/{id}")
+    public ResponseEntity<Object> deletePostById(@PathVariable Integer id){
+        return new ResponseEntity<>("Deleted Successfully Now", HttpStatus.OK);
+    }
+
+//    todo added some item that need to be captured will see how its going to be work for me
+//    started on 5:15PM
+    //  hj hjh sdfdf vgfbg bgbgh njnhj wewre fggfgb
+//    asddsfdf vgfbgf hgnh qweewreretrtr
+    // nknknk dfd vfdvd
+    // vfgbgf bgfbfg bgfbf rgert qeqweweefv  bgbhggh  bghbghh
+//    asdfghjkl lkjhgfdsa
+  
 
 }
---
+++ src/main/java/com/citiustech/blog/model/BlogDto.java
@@ -0,0 +1,9 @@
+package com.citiustech.blog.model;
+// created file 4:45
+public class BlogDto {
+    private String title;
+    private String postTest;
+
+
+
+}
